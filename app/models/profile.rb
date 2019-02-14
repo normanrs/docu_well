e59343485ebd9a757475e70992f8027c3f23ec35
normanrs <normanrs@live.com>
2019-02-14 16:54:31 -0700

    Added insurances to profile get

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,14 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  has_many :insurances
   enum blood_type: [:o_negative, :o_positive, :a_negative, :a_positive, :b_negative, :b_positive, :ab_negative, :ab_positive]
 
   def provider
     Provider.find(self.provider_id)
   end
+
+  def insurances
+    Insurance.where(profile_id: self.id)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
 FactoryBot.create(:provider)
-FactoryBot.create_list(:profile, 5)
+profile_1 = FactoryBot.create(:profile)
+profile_2 = FactoryBot.create(:profile)
+profile_3 = FactoryBot.create(:profile)
+profile_4 = FactoryBot.create(:profile)
+profile_5 = FactoryBot.create(:profile)
+FactoryBot.create(:insurance, profile_id: profile_1.id)
+FactoryBot.create(:insurance, profile_id: profile_2.id)
+FactoryBot.create(:insurance, profile_id: profile_3.id)
+FactoryBot.create(:insurance, profile_id: profile_4.id)
+FactoryBot.create(:insurance, profile_id: profile_5.id)
 
 p 'Seeds created'
