80515cd93e1aee0b28cb6b329db91d7b12fbad5d
mmbensalah <mmbensalah@gmail.com>
2019-02-14 14:12:40 -0700

    Add gem faker, add to rails helper, create factory for profile

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,9 +1,16 @@
 FactoryBot.define do
   factory :profile do
-    user
-    given_name {'Frida'}
-    surname {'Cowlow'}
-
+    given_name { Faker::Name.name }
+    surname { Faker::GreekPhilosophers.name }
+    dob { Faker::Date.birthday(5,75) }
+    height { Faker::Number.between(48, 96) }
+    weight { Faker::Number.between(5, 300) }
+    bp_systolic { Faker::Number.between(70, 160) }
+    bp_diastolic { Faker::Number.between(40, 130) }
+    heart_rate { Faker::Number.between(30, 200) }
+    blood_type { Faker::Number.between(1, 8) }
+    provider_id { 1 }
+    user_id { 1 }
   end
 
 end
