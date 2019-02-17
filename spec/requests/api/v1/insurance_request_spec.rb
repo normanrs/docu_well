a8a81661bbdfcf674eecec59932b311093f91c1d
mmbensalah <mmbensalah@gmail.com>
2019-02-17 15:30:24 -0700

    Add sad path to test

--- spec/requests/api/v1/insurance_request_spec.rb
+++ spec/requests/api/v1/insurance_request_spec.rb
@@ -33,5 +33,30 @@ describe 'the insurance endpoints' do
     expect(data[:attributes].keys.include?(:profile_id)).to be(true)
   end
 
+  it 'POST /insurances returns error w/o api key' do
+    provider  = create(:provider)
+    user      = create(:user)
+    profile   = create(:profile, user_id: user.id, provider_id: provider.id )
+
+    insurance_type = "medical"
+    carrier = "Aetna"
+    id_number = "abc123"
+    group_number = "def456"
+    phone_number = "800-111-1111"
+
+    data = {insurance_type: insurance_type,
+            carrier: carrier,
+            id_number: id_number,
+            group_number: group_number,
+            phone_number: phone_number,
+            profile_id: profile.id}
+
+    post "/api/v1/insurances", params: data
+
+    expect(response.status).to eq 422
+    data = JSON.parse(response.body)
+    expect(data["message"]).to eq("Bad API key")
+  end
+
 
 end
