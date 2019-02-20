38645cdcaa9dbcd4f7cb5852365c119aef1f885c
normanrs <normanrs@live.com>
2019-02-20 15:18:33 -0700

    Saving cleaned addresses to db

--- spec/requests/api/v1/providers_spec.rb
+++ spec/requests/api/v1/providers_spec.rb
@@ -45,6 +45,24 @@ describe 'the provider endpoint' do
     expect(data[:attributes].keys.include?(:speciality)).to be(true)
   end
 
+  it 'POST /providers creates provider and returns provider content in json' do
+    user      = create(:user)
+    data = {
+      api_key: user.api_key,
+      given_name: 'Chelsea',
+      surname: 'Manchot',
+      street_address: '0 Does not exist street',
+      city: 'Denver',
+      state: 'CO',
+      zip: '80211',
+      phone: '101-555-1212'}
+
+      post "/api/v1/providers", params: data
+      expect(response.status).to eq 400
+      data = JSON.parse(response.body, symbolize_names: true)
+      expect(data[:message]).not_to be_empty
+  end
+
   it 'POST /providers returns 400 error on missing data' do
     user      = create(:user)
     data = {
